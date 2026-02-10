0e5943632d2c87b6b6f5240cac5dd06c9c9f0123
lanzoor <lanzoorgaming@gmail.com>
2026-02-10 20:36:02 +0900

    FATAL

--- api/ping.ts
+++ api/ping.ts
@@ -3,11 +3,11 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
     console.log('Ping invoked', { method: req.method, url: req.url });
 
-    const body = JSON.stringify({
+    const body = {
         ok: true,
         message: 'pong!',
         time: Date.now(),
-    });
+    };
 
     res.status(200).json(body);
 }
