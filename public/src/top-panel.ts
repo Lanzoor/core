72626bb6ba6ef536555033e66e28a31ac304135e
lanzoor <lanzoorgaming@gmail.com>
2026-02-15 21:00:20 +0900

    Stage commits for v26.7.1: fix panel to use relative units only when necessary, and update top panel layout & functionality

--- public/src/top-panel.ts
+++ public/src/top-panel.ts
@@ -1,43 +1,18 @@
 const topPanel = document.createElement('div');
 topPanel.id = 'top-panel';
+
 document.body.appendChild(topPanel);
 
-class TopPanelDestination {
-    public displayName: string;
-    public link: string;
-    public identifier?: string;
+topPanel.innerHTML = `
+<a id="panel-logo" href="/index.html">
+    lanzoor.dev
+</a>
 
-    constructor(displayName: string, link: string, identifier?: string) {
-        this.displayName = displayName;
-        this.link = link;
-        this.identifier = identifier;
-    }
+<div id="panel-navigation">
+    <p>Navigation</p>
 
-    format() {
-        const idAttr = this.identifier ? `id="${this.identifier}" ` : '';
-        return `<td>
-    <a ${idAttr}href="${this.link}">${this.displayName}</a>
-</td>`;
-    }
-}
-
-const destinations: TopPanelDestination[] = [
-    //
-    new TopPanelDestination('lanzoor.dev', '/index.html', 'large-text'),
-    new TopPanelDestination('Welcome!', '/index.html'),
-    new TopPanelDestination('Profile', '/profile/index.html'),
-    new TopPanelDestination('Credits', '/credits/index.html'),
-    new TopPanelDestination('Documents', '/docs/index.html'),
-    new TopPanelDestination('Announcements', '/docs/announcements/index.html'),
-    new TopPanelDestination('Troubleshooting', '/troubleshooting/index.html'),
-];
-
-const tableHTML = `
-    <table>
-        <tr>
-            ${destinations.map((dest) => dest.format()).join('')}
-        </tr>
-    </table>
+    <button>
+        <img src="/assets/icons/hamburger.svg" alt="" id="hamburger-icon">
+    </button>
+</div>
 `;
-
-topPanel.innerHTML = tableHTML;
