8f78f2d5690e7049948fb5cf616bf0a8aedd924f
lanzoor <lanzoorgaming@gmail.com>
2026-02-27 15:39:04 +0900

    Stage commits for v26.9.0: Add Discord status display, and update backend code to be versatile

--- public/src/ping-server.ts
+++ public/src/ping-server.ts
@@ -2,6 +2,7 @@ async function pingServer(route: string): Promise<any | undefined> {
     try {
         const res = await fetch(route);
 
+        console.log(res);
         if (!res.ok) {
             console.error('Ping API failed:', res.status, res.statusText);
             return;
