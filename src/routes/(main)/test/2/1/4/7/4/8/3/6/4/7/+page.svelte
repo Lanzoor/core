ffe627dfbc8f18f527cab6489d787c24fb9b4352
Lanzoor <lanzoor-programming@proton.me>
2026-09-20 22:08:10 +0900

    w

---
+++ src/routes/(main)/test/2/1/4/7/4/8/3/6/4/7/+page.svelte
@@ -0,0 +1,131 @@
+<svelte:head>
+    <title>건강한 식생활 실천하기</title>
+</svelte:head>
+
+<section class="noto-sans" lang="ko">
+    <h1>기가 수행 1-2.2: 건강한 식생활 실천하기</h1>
+
+    <h2>시험 정보</h2>
+
+    <ul>
+        <li>
+            <b>평가 만점</b>: 25점
+        </li>
+        <li>
+            <b>평가 시기</b>: 9월 21일 월요일 5교시
+        </li>
+    </ul>
+
+    <h3>평가 요소</h3>
+
+    <ul>
+        <li>
+            당류의 구조와 과도한 당분 섭취가 겅강에 미치는 영향을 구체적이고
+            정확하게 서술함.
+        </li>
+        <li>환경호르몬의 작용 원리와 환경호르몬이 건강에 미치는 영향을 〃.</li>
+        <li>
+            당류와 환경호르몬 노출을 줄이기 위한 실천 방안을 각각 논리적이고
+            설득력 있게 제시함.
+        </li>
+    </ul>
+
+    <h2>당류의 구조</h2>
+
+    <ul>
+        <li>
+            설탕 (sucrose) 은 <span class="amgi">포도당 (glucose)</span>과
+            <span class="amgi">과당 (fructose)</span>의
+            <span class="amgi">화학적 결합</span>이다.
+        </li>
+        <li>
+            유당 (lactose) 은 <span class="amgi">포도당 (glucose)</span>과
+            <span class="amgi">갈락토오스 (galactose)</span>의
+            <span class="amgi">화학적 결합</span>이다.
+        </li>
+        <li>
+            액상과당 (HFCS) 은 <span class="amgi">포도당 (glucose)</span>과
+            <span class="amgi">과당 (fructose)</span>의
+            <span class="amgi">단순 혼합물</span>이다.
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            구조 형태가 화학적 결합이다? 소화 과정이 <span class="amgi"
+                >필요하기</span
+            >
+            때문에 우리 몸에 <span class="amgi">천천히</span> 흡수된다.
+        </li>
+        <li>
+            구조 형태가 단순 혼합물이다? 소화 과정이 <span class="amgi"
+                >필요하지 않기</span
+            >
+            때문에 우리 몸에 <span class="amgi">빠르게</span> 흡수된다.
+        </li>
+    </ul>
+
+    <h1>액상과당을 과다 섭취하면...</h1>
+
+    <ol>
+        <li class="amgi">많은 양의 과당이 간세포로 빠르게 들어온다.</li>
+        <li class="amgi">
+            간세포는 과당을 포도당으로 미처 다 바꾸지 못하면, 지방으로 강제
+            전환하여 쌓인다.
+        </li>
+        <li class="amgi">지방간 등과 같은 우리 몸에 악영향을 미친다.</li>
+    </ol>
+
+    <h1>이런 과당이 혈액으로 배출되면...??</h1>
+
+    <ol>
+        <li class="amgi">혈액이 끈적끈적해져 혈액 순환이 느려진다.</li>
+        <li class="amgi">
+            과당과 단백질이 결합하여 몸에 염증을 유발할 수 있다.
+        </li>
+    </ol>
+
+    <h1>당류 섭취를 줄이기 위한 실천 방안</h1>
+
+    <ul>
+        <li>식품을 살 때는 영양성분표를 읽고 산다.</li>
+        <li>음료를 마실 때는 당이 들어간 음료보다 물을 선택한다.</li>
+        <li>간식을 고를 때는 당류가 적게 들어 있는 제품을 선택한다.</li>
+    </ul>
+
+    <h1>환경 호르몬</h1>
+
+    <p>
+        이란 <span class="dim"
+            >체내 호르몬을 모방하거나 그 작용을 방해하여, 내분비계의 정상적인
+            기능을 교란할 수 있는 물질</span
+        >
+    </p>
+
+    <p>
+        예시: <span class="amgi">비스페놀 A</span>,
+        <span class="amgi">프탈레이트</span> 등
+    </p>
+
+    <p class="amgi">
+        면역 시스템 붕괴, 성조숙증 (또래보다 2차 성징이 빨리 나타나는 것), 생식
+        기능 저하 등의 악영향을 미칠 수 있음
+    </p>
+
+    <ul>
+        <li>편의점에서 받은 영수증을 손으로 만질 때</li>
+        <li>플라스틱 물병을 햇빛이나 높은 온도에 오래 두었을 때</li>
+        <li>뜨거운 음식을 일회용 플라스틱 용기에 담을 때</li>
+    </ul>
+
+    <h1>환경호르몬 노출을 줄이기 위한 실천 방안</h1>
+
+    <ul>
+        <li>
+            과일이나 채소를 세척할 때에는 향이 강한 세제 대신 흐르는 물로
+            씻는다.
+        </li>
+        <li>영수증을 보관해야 한다면 반으로 접어서 뒷면으로 만진다.</li>
+        <li>뜨거운 음식은 플라스틱 용기보다 유리나 도자기 용기에 담는다.</li>
+    </ul>
+</section>
